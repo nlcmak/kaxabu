32e0f6b98ed4058e6639166dd81195793dae4feb
Aaron <Aaron@192.168.1.102>
2013-12-17 23:27:05 +0800

    Original 醜男

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaxabu::Application.config.secret_key_base = '3cf7d269beb6857dff015e103b95602bac9efd7448dca9a95e296e4f3a79a8c67f30cf998a5733d999e5744135529a62f1f0c631d3f9794ebeb9c91c2f07b4e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kaxabu::Application.config.session_store :cookie_store, key: '_kaxabu_session'
