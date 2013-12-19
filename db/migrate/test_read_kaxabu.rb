aaee4c5200529691782bd1e45e67f78857de78e7
LEO <u9761130@oz.nthu.edu.tw>
2013-12-19 17:11:13 +0800

    增加注意事項 Memo.txt 醜男

--- db/migrate/test_read_kaxabu.rb
+++ db/migrate/test_read_kaxabu.rb
@@ -1,18 +1,23 @@
 ﻿# encoding: UTF-8
 require 'roo'
-require 'xlsx_writer'
-require 'fileutils'
 
-
-s = Roo::Excelx.new('migrate_data\\噶哈巫語分類辭典.xlsx')
+s = Roo::Excelx.new('migrate_data\\test.xlsx')
 s.default_sheet = s.sheets[0]
-doc = XlsxWriter.new
-sheet1 = doc.add_sheet("sheet1")
 
-s.each do |e|
-  sheet1.add_row [e.to_s]
+File.open('migrate_data\\噶哈巫語分類辭典_out.txt','w') do |file|
+  lui7 = nil
+  (3..s.last_row).each do |i|
+    s.row(i).each_with_index do |content, index|
+      content = content.to_s.strip
+      case index
+      when 0
+        if content.empty? == false
+          lui7 = content.split('.')[1]
+        end
+        content = lui7
+      end
+      file.write content+', '
+    end
+    file.write "\n"
+  end
 end
-
-puts doc.path
-FileUtils.mv doc.path, 'migrate_data\\噶哈巫語分類辭典_out.xlsx'
-doc.cleanup
