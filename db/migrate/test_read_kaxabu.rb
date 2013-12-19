98ca68b6ea6a49ac76ce017a62ea69d44d2982a3
LEO <u9761130@oz.nthu.edu.tw>
2013-12-19 17:28:22 +0800

    修改辭典parser 醜男

--- db/migrate/test_read_kaxabu.rb
+++ db/migrate/test_read_kaxabu.rb
@@ -5,16 +5,20 @@ s = Roo::Excelx.new('migrate_data\\test.xlsx')
 s.default_sheet = s.sheets[0]
 
 File.open('migrate_data\\噶哈巫語分類辭典_out.txt','w') do |file|
+  pinn1mia5 = nil
   lui7 = nil
   (3..s.last_row).each do |i|
     s.row(i).each_with_index do |content, index|
-      content = content.to_s.strip
+      content = content.to_s.gsub '　',' '
+      content.strip!
       case index
       when 0
         if content.empty? == false
-          lui7 = content.split('.')[1]
+          pinn1mia5 = content.split('.')[1]
         end
-        content = lui7
+        content = pinn1mia5
+      when 1
+        lui7
       end
       file.write content+', '
     end
